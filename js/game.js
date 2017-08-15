0aefb866526d5f85c124ea64522d778f73b25a55
Frank Mitchell <me@frankmitchell.org>
2017-08-15 18:31:19 -0400

    Render spells to the top of the playing area.

--- js/game.js
+++ js/game.js
@@ -1,8 +1,50 @@
 ;(function (Game) {
 'use strict';
 
+var Spells = (function () {
+'use strict';
+
+var $ = window.jQuery
+  , dirty = 0
+  , casted = []
+  , spells = {}
+
+spells.reset = function () {
+  casted = []
+  dirty |= 1
+}
+
+spells.render = function () {
+  if (dirty & 1) {
+    var html = ''
+
+    for (var spell of casted) {
+      html += '<p class="spell">'
+      html += '<span class="name">'+spell.name+'</span>'
+      html += '<span class="power">'+spell.value+'</span>'
+      html += '<span class="gem '+spell.suit1+'"></span>'
+      html += '<span class="gem '+spell.suit2+'"></span>'
+      html += '</p>'
+    }
+
+    $('#spells').html(html)
+  }
+
+  dirty = 0
+}
+
+spells.cast = function () {
+  casted.push({name: 'Magic Missile', value: 3, suit1: 'suns', suit2: 'moons'})
+  dirty |= 1
+}
+
+return spells
+}())
+
 function render () {
   requestAnimationFrame(render)
+
+  Spells.render()
 }
 
 function startGame (callback) {
@@ -10,10 +52,8 @@ function startGame (callback) {
 }
 
 Game.play = function () {
-  var $ = window.jQuery
-    , html = '<div id="deck"></div>'
-
-  $('#world').html(html)
+  Spells.cast()
+  Spells.cast()
 
   startGame(render)
 }
