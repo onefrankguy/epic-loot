4f9451ad5b1195f9052ccde7ee2da092e99d1e2c
Frank Mitchell <me@frankmitchell.org>
2017-08-15 22:03:39 -0400

    Show random signs that can be selected.

--- js/game.js
+++ js/game.js
@@ -138,6 +138,285 @@ deck.deal = function () {
 return deck
 }())
 
+var Signpost = (function () {
+
+function makePersonalities () {
+  var cards = [{
+    name: 'the Author',
+    value: 2,
+    suit1: 'moons',
+    suit2: 'knots'
+  },{
+    name: 'the Painter',
+    value: 3,
+    suit1: 'suns',
+    suit2: 'knots'
+  },{
+    name: 'the Savage',
+    value: 3,
+    suit1: 'leaves',
+    suit2: 'wyrms'
+  },{
+    name: 'the Lunatic',
+    value: 6,
+    suit1: 'moons',
+    suit2: 'waves'
+  },{
+    name: 'the Penitent',
+    value: 6,
+    suit1: 'suns',
+    suit2: 'wyrms'
+  },{
+    name: 'the Merchant',
+    value: 9,
+    suit1: 'leaves',
+    suit2: 'knots'
+  },{
+    name: 'the Watchman',
+    value: 11,
+    suit1: 'moons',
+    suit2: 'wyrms',
+    suit3: 'knots'
+  },{
+    name: 'the Light Keeper',
+    value: 11,
+    suit1: 'suns',
+    suit2: 'waves',
+    suit3: 'knots'
+  },{
+    name: 'the Consul',
+    value: 12,
+    suit1: 'moons',
+    suit2: 'waves',
+    suit3: 'knots'
+  },{
+    name: 'the Bard',
+    value: 10,
+    suit1: 'suns'
+  },{
+    name: 'the Huntress',
+    value: 10,
+    suit1: 'moons'
+  }]
+
+  PRNG.shuffle(cards)
+  return cards
+}
+
+function makeEvents () {
+  var cards = [{
+    name: 'the Journey',
+    value: 3,
+    suit1: 'moons',
+    suit2: 'waves'
+  },{
+    name: 'Battle',
+    value: 4,
+    suit1: 'wyrms',
+    suit2: 'knots'
+  },{
+    name: 'Discovery',
+    value: 5,
+    suit1: 'suns',
+    suit2: 'waves'
+  },{
+    name: 'the Market',
+    value: 6,
+    suit1: 'leaves',
+    suit2: 'knots'
+  },{
+    name: 'the Chance Meeting',
+    value: 7,
+    suit1: 'moons',
+    suit2: 'leaves'
+  },{
+    name: 'Betrayal',
+    value: 8,
+    suit1: 'wyrms',
+    suit2: 'knots'
+  },{
+    name: 'the Pact',
+    value: 9,
+    suit1: 'moons',
+    suit2: 'suns'
+  },{
+    name: 'the Harvest',
+    value: 11,
+    suit1: 'moons',
+    suit2: 'suns',
+    suit3: 'leaves'
+  },{
+    name: 'the Rite',
+    value: 12,
+    suit1: 'moons',
+    suit2: 'leaves',
+    suit3: 'wyrms'
+  },{
+    name: 'Calamity',
+    value: 10,
+    suit1: 'wyrms'
+  },{
+    name: 'Windfall',
+    value: 10,
+    suit1: 'knots'
+  }]
+
+  PRNG.shuffle(cards)
+  return cards
+}
+
+function makeLocations () {
+  var cards = [{
+    name: 'the Desert',
+    value: 2,
+    suit1: 'suns',
+    suit2: 'wyrms'
+  },{
+    name: 'the Mountain',
+    value: 4,
+    suit1: 'moons',
+    suit2: 'suns'
+  },{
+    name: 'the Forest',
+    value: 5,
+    suit1: 'moons',
+    suit2: 'leaves'
+  },{
+    name: 'the Castle',
+    value: 7,
+    suit1: 'suns',
+    suit2: 'knots'
+  },{
+    name: 'the Cave',
+    value: 7,
+    suit1: 'waves',
+    suit2: 'wyrms'
+  },{
+    name: 'the Mill',
+    value: 8,
+    suit1: 'waves',
+    suit2: 'leaves'
+  },{
+    name: 'the Darkness',
+    value: 9,
+    suit1: 'waves',
+    suit2: 'wyrms'
+  },{
+    name: 'the Borderland',
+    value: 11,
+    suit1: 'waves',
+    suit2: 'leaves',
+    suit3: 'wyrms'
+  },{
+    name: 'the Island',
+    value: 12,
+    suit1: 'suns',
+    suit2: 'waves',
+    suit3: 'wyrms'
+  },{
+    name: 'the Window',
+    value: 12,
+    suit1: 'suns',
+    suit2: 'leaves',
+    suit3: 'knots'
+  },{
+    name: 'the Sea',
+    value: 10,
+    suit1: 'waves'
+  }]
+
+  PRNG.shuffle(cards)
+
+  return [].concat([{
+    name: 'the Origin',
+    value: 2,
+    suit1: 'waves',
+    suit2: 'leaves'
+  }], cards, [{
+    name: 'the End',
+    value: 10,
+    suit1: 'knots'
+  }])
+}
+
+function makeSignHTML (sign) {
+  var html = ''
+  html += '<span class="name">'+sign.name+'</span>'
+  html += '<span class="value">'+sign.value+'</span>'
+  if (sign.suit1) {
+    html += '<span class="gem '+sign.suit1+'"></span>'
+  }
+  if (sign.suit2) {
+    html += '<span class="gem '+sign.suit2+'"></span>'
+  }
+  if (sign.suit3) {
+    html += '<span class="gem '+sign.suit3+'"></span>'
+  }
+  return html
+}
+
+var $ = window.jQuery
+  , dirty = 1
+  , personalities = makePersonalities()
+  , personalities_index = 0
+  , events = makeEvents()
+  , events_index = 0
+  , locations = makeLocations()
+  , locations_index = 0
+  , selected = undefined
+  , stage = 1
+  , signpost = {}
+
+signpost.reset = function () {
+  personalities = makePersonalities()
+  personalities_index = 0
+  events = makeEvents()
+  events_index = 0
+  locations = makeLocations()
+  locations_index = 0
+  selected = undefined
+  stage = 1
+  dirty = 1
+}
+
+signpost.render = function () {
+  if (dirty & 1) {
+    var sign1 = {name: '', value: ''}
+      , sign2 = {name: '', value: ''}
+
+    if (stage === 1) {
+      sign1 = personalities[personalities_index]
+      sign2 = events[events_index]
+    }
+
+    if (stage === 2) {
+      sign1 = locations[locations_index]
+    }
+
+    $('#sign1').html(makeSignHTML(sign1))
+    $('#sign2').html(makeSignHTML(sign2))
+  }
+
+  if (dirty & 2) {
+    if (selected === 'sign1') {
+      $('#sign2').html('')
+    }
+    if (selected === 'sign2') {
+      $('#sign1').html('')
+    }
+  }
+
+  dirty = 0
+}
+
+signpost.select = function (sign) {
+  selected = sign
+  dirty |= 2
+}
+
+return signpost
+}())
+
 var Spells = (function () {
 'use strict';
 
@@ -154,7 +433,7 @@ spells.reset = function () {
 spells.render = function () {
   if (dirty & 1) {
     var html = ''
-      , visible = casted.slice(-15)
+      , visible = casted.slice(-11)
 
     for (var spell of visible) {
       html += '<p class="spell">'
@@ -289,10 +568,16 @@ function offGem (element) {
   }
 }
 
+function offSign (element) {
+  var type = element.unwrap().id
+  Signpost.select(type)
+}
+
 function render () {
   requestAnimationFrame(render)
 
   Deck.render()
+  Signpost.render()
   Spells.render()
   Gems.render()
 }
@@ -310,6 +595,9 @@ Game.play = function () {
   $('#leaves').touch(undefined, offGem)
   $('#wyrms').touch(undefined, offGem)
   $('#knots').touch(undefined, offGem)
+
+  $('#sign1').touch(undefined, offSign)
+  $('#sign2').touch(undefined, offSign)
 
   startGame(render)
 }
