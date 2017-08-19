f2d96540ddca0d77a6cae623540876eb3c632130
Frank Mitchell <me@frankmitchell.org>
2017-08-19 14:03:36 -0400

    Move jQuery code into the same file as game code for better docs.

--- js/game.js
+++ js/game.js
@@ -430,7 +430,6 @@ const Tokens = (function tokens() {
 }());
 
 const Game = (function game() {
-  const $ = window.jQuery;
   let color;
   let played = [];
   let dirty = true;
@@ -496,6 +495,7 @@ const Game = (function game() {
   }
 
   function renderDeck() {
+    const $ = window.jQuery;
     let html = '';
 
     played.forEach((name) => {
@@ -511,12 +511,14 @@ const Game = (function game() {
   }
 
   function renderObstacles() {
+    const $ = window.jQuery;
     const obstacles = Obstacles.get();
     $('#sign1').html(renderCard(obstacles[0]));
     $('#sign2').html(renderCard(obstacles[1]));
   }
 
   function renderTokens() {
+    const $ = window.jQuery;
     const tokens = Tokens.get();
 
     Object.keys(tokens).forEach((type) => {
@@ -589,6 +591,8 @@ const Game = (function game() {
   }
 
   function play() {
+    const $ = window.jQuery;
+
     $('#moons').touch(undefined, onToken);
     $('#suns').touch(undefined, onToken);
     $('#waves').touch(undefined, onToken);
@@ -607,6 +611,96 @@ const Game = (function game() {
   return {
     play,
   };
+}());
+
+(function $() {
+  function Fn(selector) {
+    if (selector instanceof Fn) {
+      return selector;
+    }
+
+    this.element = selector;
+
+    if (typeof selector === 'string') {
+      if (selector.indexOf('#') === 0) {
+        this.element = document.getElementById(selector.slice(1));
+      }
+    }
+
+    return this;
+  }
+
+  Fn.prototype.html = function html(value) {
+    if (this.element) {
+      if (value === undefined) {
+        return this.element.innerHTML;
+      }
+
+      this.element.innerHTML = value;
+    }
+
+    return this;
+  };
+
+  Fn.prototype.on = function on(message, callback) {
+    if (this.element) {
+      this.element.addEventListener(message, callback, false);
+    }
+
+    return this;
+  };
+
+  Fn.prototype.touch = function touch(start, end) {
+    const self = this;
+
+    if (this.element) {
+      if ('ontouchstart' in document.documentElement === false) {
+        this.element.onmousedown = function onmousedown(mouseDownEvent) {
+          if (start) {
+            start(self, mouseDownEvent);
+          }
+          document.onmousemove = function onmousemove(e) {
+            e.preventDefault();
+          };
+          document.onmouseup = function onmouseup(e) {
+            if (end) {
+              end(self, e);
+            }
+            document.onmousemove = undefined;
+            document.onmouseup = undefined;
+          };
+        };
+      } else {
+        this.element.ontouchstart = function ontouchstart(touchStartEvent) {
+          if (start) {
+            start(self, touchStartEvent);
+          }
+          document.ontouchmove = function ontouchmove(e) {
+            e.preventDefault();
+          };
+          document.ontouchend = function ontouchend(e) {
+            if (end) {
+              end(self, e);
+            }
+            document.ontouchmove = undefined;
+            document.ontouchend = undefined;
+          };
+        };
+      }
+    }
+
+    return this;
+  };
+
+  Fn.prototype.unwrap = function unwrap() {
+    return this.element;
+  };
+
+  function root(selector) {
+    return new Fn(selector);
+  }
+
+  window.jQuery = root;
 }());
 
 Game.play();
