959f36431fe152584c0e1fcc37db3d826860ae34
Frank Mitchell <me@frankmitchell.org>
2017-08-18 22:41:48 -0400

    Fix lint errors in jQuery code.

--- js/jquery.js
+++ js/jquery.js
@@ -1,91 +1,89 @@
-;(function () {
-'use strict';
-
-function Fn (selector) {
-  if (selector instanceof Fn) {
-    return selector
-  }
-
-  this.element = selector
-
-  if (typeof selector === 'string') {
-    if (selector.indexOf('#') === 0) {
-      this.element = document.getElementById(selector.slice(1))
-    }
-  }
-
-  return this
-}
-
-Fn.prototype.html = function (value) {
-  if (this.element) {
-    if (value === undefined) {
-      return this.element.innerHTML
+(function () { // eslint-disable-line func-names
+  function Fn(selector) {
+    if (selector instanceof Fn) {
+      return selector;
     }
 
-    this.element.innerHTML = value
-  }
+    this.element = selector;
 
-  return this
-}
-
-Fn.prototype.on = function (message, callback) {
-  if (this.element) {
-    this.element.addEventListener(message, callback, false)
-  }
-
-  return this
-}
-
-Fn.prototype.touch = function (start, end) {
-  var self = this
-
-  if (this.element) {
-    if ('ontouchstart' in document.documentElement === false) {
-      this.element.onmousedown = function (e) {
-        if (start) {
-          start(self, e)
-        }
-        document.onmousemove = function (e) {
-          e.preventDefault()
-        }
-        document.onmouseup = function (e) {
-          if (end) {
-            end(self, e)
-          }
-          document.onmousemove = undefined
-          document.onmouseup = undefined
-        }
-      }
-    } else {
-      this.element.ontouchstart = function (e) {
-        if (start) {
-          start(self, e)
-        }
-        document.ontouchmove = function (e) {
-          e.preventDefault()
-        }
-        document.ontouchend = function (e) {
-          if (end) {
-            end(self, e)
-          }
-          document.ontouchmove = undefined
-          document.ontouchend = undefined
-        }
+    if (typeof selector === 'string') {
+      if (selector.indexOf('#') === 0) {
+        this.element = document.getElementById(selector.slice(1));
       }
     }
+
+    return this;
   }
 
-  return this
-}
+  Fn.prototype.html = function html(value) {
+    if (this.element) {
+      if (value === undefined) {
+        return this.element.innerHTML;
+      }
 
-Fn.prototype.unwrap = function () {
-  return this.element
-}
+      this.element.innerHTML = value;
+    }
 
-function root (selector) {
-  return new Fn(selector)
-}
+    return this;
+  };
 
-window.jQuery = root
-})()
+  Fn.prototype.on = function on(message, callback) {
+    if (this.element) {
+      this.element.addEventListener(message, callback, false);
+    }
+
+    return this;
+  };
+
+  Fn.prototype.touch = function touch(start, end) {
+    const self = this;
+
+    if (this.element) {
+      if ('ontouchstart' in document.documentElement === false) {
+        this.element.onmousedown = function onmousedown(mouseDownEvent) {
+          if (start) {
+            start(self, mouseDownEvent);
+          }
+          document.onmousemove = function onmousemove(e) {
+            e.preventDefault();
+          };
+          document.onmouseup = function onmouseup(e) {
+            if (end) {
+              end(self, e);
+            }
+            document.onmousemove = undefined;
+            document.onmouseup = undefined;
+          };
+        };
+      } else {
+        this.element.ontouchstart = function ontouchstart(touchStartEvent) {
+          if (start) {
+            start(self, touchStartEvent);
+          }
+          document.ontouchmove = function ontouchmove(e) {
+            e.preventDefault();
+          };
+          document.ontouchend = function ontouchend(e) {
+            if (end) {
+              end(self, e);
+            }
+            document.ontouchmove = undefined;
+            document.ontouchend = undefined;
+          };
+        };
+      }
+    }
+
+    return this;
+  };
+
+  Fn.prototype.unwrap = function unwrap() {
+    return this.element;
+  };
+
+  function root(selector) {
+    return new Fn(selector);
+  }
+
+  window.jQuery = root;
+}());
