ece13fabf0e615a0fbf11cef10cc04c938e9b831
Frank Mitchell <me@frankmitchell.org>
2017-08-28 05:48:09 -0400

    Show number of bytes remaining in the zip file.

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 const fs = require('fs');
+
 const max = 13 * 1024;
 
 fs.stat('game.zip', (err, stats) => {
@@ -12,9 +14,12 @@ fs.stat('game.zip', (err, stats) => {
   const bytes = stats.size;
   const percent = Math.round((bytes * 100) / max);
   console.log(`${bytes} bytes (used ${percent}%)`);
+  console.log(`${max - bytes} bytes remaining`);
 
   if (bytes > max) {
-    console.error('zip file too big!');
+    console.error('zip file is too big!');
     process.exit(1);
   }
 });
+
+/* eslint-enable no-console */
